34706d9a2eed69745c759d8274f8f00076de7379
wangzhenfu <wangzhenfu@zto.com>
2020-04-01 10:49:06 +0800

    update emms

--- user-config/init-emms.el
+++ user-config/init-emms.el
@@ -2,11 +2,17 @@
 (add-to-list 'load-path "~/.emacs.d/emms/lisp")
 (require 'emms-setup)
 (require 'emms-player-vlc)
-(emms-standard)
+;;(emms-standard) ;; (emms-devel)
+(emms-all)
 (emms-default-players)
+
+(setq emms-source-file-default-directory "~/Documents/music/"
+      emms-info-asynchronously t
+      emms-show-format "♪ %s")
+
 (setq emms-player-vlc-command-name
       "/Applications/VLC.app/Contents/MacOS/VLC")
 (setq emms-player-list '(
-			 emms-player-vlc))
+                         emms-player-vlc))
 
 (provide 'init-emms)
